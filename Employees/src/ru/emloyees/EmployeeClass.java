a77dfab00d277def5aaa28625bdcff7bd6517f49
anasochenkova <anasochenkova@gmail.com>
2017-10-14 00:02:33 +0300

    Employee Task
    Задание на реализацию интерфейса в классе.

---
+++ Employees/src/ru/emloyees/EmployeeClass.java
@@ -0,0 +1,11 @@
+package ru.emloyees;
+
+public class EmployeeClass implements Employee {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String position;
+	private int salary = 300;
+	private Employee manager;
+
+}
